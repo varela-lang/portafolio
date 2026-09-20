5e4fea86dcd99cbb6b32b8264680715ec550a267
sergio varela <sergiovarela355@gmail.com>
2026-09-20 17:48:43 -0600

    Portafolio Actualizado

--- portafolio/js/main.js
+++ portafolio/js/main.js
@@ -1,9 +1,9 @@
 
 const typed = new Typed('.typed', {
-    strings: [
-    '<i class="texto">Full Stack</i>', 
-    '<i class="texto">Developer</i>'],
-    stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	strings: [
+		'<i class="texto">Full Stack</i>',
+		'<i class="texto">Developer</i>'],
+	stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
 	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
 	startDelay: 150, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
 	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
@@ -17,14 +17,8 @@ const typed = new Typed('.typed', {
 	contentType: 'html', // 'html' o 'null' para texto sin formato
 });
 
-function alerta() {
-	alert("Hola")
-}
 
-
- 
-
-  var swiper = new Swiper(".slide-content", {
+var swiper = new Swiper(".slide-content", {
 	effect: "cards",
 	grabCursor: true,
-  });
+});
